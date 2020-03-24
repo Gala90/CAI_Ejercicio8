be1557545466a74f5d3e46099f8b98bb9d8ba4e1
Gala <galamagdalena@hotmail.com>
2020-03-24 18:26:24 -0300

    Ejercicio8

---
+++ Ejercicio8/Ejercicio8/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+using System.Globalization;
+
+namespace Ejercicio8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string dato;
+            DateTime date1;
+          
+
+            Console.WriteLine("Ingrese una fecha en formato DD/MM/AAAA: ");
+            dato = Console.ReadLine();
+
+            date1 = Convert.ToDateTime(dato);
+
+                                  
+            DateTime fechaHoy = DateTime.Now;
+
+        
+            TimeSpan t = date1 - fechaHoy;
+            int NrOfDays = t.Days;
+
+            Console.WriteLine(NrOfDays);
+
+
+            Console.ReadKey();
+        }
+    }
+}
